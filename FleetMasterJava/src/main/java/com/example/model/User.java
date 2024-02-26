372b8b76a9ccccef648a8b8ef17c8b2582c7f1ba
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-02-26 15:16:58 +0100

    added user, validation dependency, userDto

---
+++ FleetMasterJava/src/main/java/com/example/model/User.java
@@ -0,0 +1,24 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @Email
+    private String email;
+
+}
---
+++ FleetMasterJava/src/main/java/com/example/model/dto/UserDto.java
@@ -0,0 +1,4 @@
+package com.example.model.dto;
+
+public record UserDto(int id, String name, String email) {
+}
