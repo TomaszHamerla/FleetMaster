a9548a541e5a804a9aa3d2117439958c2842b368
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-02-27 09:44:46 +0100

    added passwordEncoder

--- FleetMasterJava/src/main/java/com/example/security/SecurityConfiguration.java
+++ FleetMasterJava/src/main/java/com/example/security/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +22,9 @@ public class SecurityConfiguration {
                 )
                 .csrf(AbstractHttpConfigurer::disable) // no problem to put/patch methods
                 .build();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12);
     }
 }
