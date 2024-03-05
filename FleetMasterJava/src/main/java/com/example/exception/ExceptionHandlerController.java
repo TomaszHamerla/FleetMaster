c70394223a9432701ab8acbcd4378714f30c7f23
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-03-05 15:44:42 +0100

    added handler to handle problem with creating user with username is bigger then 255 chars, fixed problem with status 200 when request was call without any auth

--- FleetMasterJava/src/main/java/com/example/exception/ExceptionHandlerController.java
+++ FleetMasterJava/src/main/java/com/example/exception/ExceptionHandlerController.java
@@ -1,11 +1,13 @@
 package com.example.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -24,35 +26,31 @@ public class ExceptionHandlerController {
         return new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
     }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class, DataIntegrityViolationException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     ErrorResponse handleMethodArgumentNotValidException(Exception e) {
         log.error(e.getMessage());
         return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
 
-    @ExceptionHandler({UsernameNotFoundException.class, BadCredentialsException.class})
+    @ExceptionHandler(BadCredentialsException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     ErrorResponse handleAuthenticationException(Exception e) {
         log.error(e.getMessage());
         return new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), "username or password is incorrect. " + e.getMessage());
     }
 
-    @ExceptionHandler(AccountStatusException.class)
+    @ExceptionHandler({UsernameNotFoundException.class, AccountStatusException.class, InsufficientAuthenticationException.class, InvalidBearerTokenException.class})
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    ErrorResponse handleAccountStatusException(AccountStatusException ex) {
-        return new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), ex.getMessage());
-    }
-
-    @ExceptionHandler(InvalidBearerTokenException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    ErrorResponse handleInvalidBearerTokenException(InvalidBearerTokenException ex) {
+    ErrorResponse handleAccountStatusException(Exception ex) {
+        log.error(ex.getMessage());
         return new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), ex.getMessage());
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     ErrorResponse handleAccessDeniedException(AccessDeniedException ex) {
+        log.error(ex.getMessage());
         return new ErrorResponse(HttpStatus.FORBIDDEN.value(), ex.getMessage());
     }
 }
