b5d4155acee8643a484f7be2d60dc1e24c5f1496
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-03-24 09:52:48 +0100

    created returnCar abs

--- FleetMasterJava/src/main/java/com/example/service/interfaces/RentCarService.java
+++ FleetMasterJava/src/main/java/com/example/service/interfaces/RentCarService.java
@@ -1,7 +1,9 @@
 package com.example.service.interfaces;
 
+import com.example.model.car.CarReturnResult;
 import com.example.model.user.User;
 
 public interface RentCarService {
     User rentCar(int userId, int brandId, int modelId);
+    CarReturnResult returnCar(int userId, int carId);
 }
