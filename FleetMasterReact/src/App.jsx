0ee1ec7fbd8cc68f83440358450d136128cfb68b
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-04-09 18:40:58 +0200

    created homepage

--- FleetMasterReact/src/App.jsx
+++ FleetMasterReact/src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import LoginForm from "./Login/LoginForm";
+import LoginForm from "./login/LoginForm";
+import { Homepage } from "./homepage/Homepage";
 
 function App() {
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
     const token = sessionStorage.getItem("Token");
@@ -25,9 +25,12 @@ function App() {
       <div className="left-background"></div>
       <div className="right-background"></div>
       <div className="center-background">
-      {isLoggedIn ? (
-          <div className="logout">
-            <button onClick={handleLogout}>Logout</button>            
+        {isLoggedIn ? (
+          <div>
+            <div className="logout">
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+            <Homepage/>
           </div>
         ) : (
           <LoginForm onLogin={handleLogin} />
---
+++ FleetMasterReact/src/homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Homepage = () => {
+  return (
+    <>
+    <div>Samochody</div>
+    <div>Wypożyczenia</div>
+    <div>Płatności</div>
+    </>
+  )
+}
